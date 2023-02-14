c1f6cc251c3e14c97412ac7e8860152ea75cd957
Charles <ckelly218@gmail.com>
2023-02-13 23:22:19 -0800

    Fix computerTurn function to iterate correctly and add index to end of array

--- index.js
+++ index.js
@@ -8,29 +8,34 @@ let rounds = 1;
 let count = 0;
 let clickCount;
 let click;
-let nextTurn = false;
-let playerTurn1 = false;
-let computerTurn;
 
-const cubeArray = [];
+let cubeArray = [];
 let cubeArrayCopy = [];
+newCube = cubes[Math.floor(Math.random() * cubes.length)];
 
-// const compareArrays = (a, b) =>
-//   a.length === b.length && a.every((v, i) => v === b[i]);
-
-function compareArrays(array1, array2) {
-  if (array1.length !== array2.length) {
+function arraysAreEqual(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    console.log("failure");
+    notice.innerText = "Sorry, try again";
+    // startButton.style.display = 'none'
     return false;
   }
-  for (let i = 0; i < cubeArray.length; i++) {
-    if (array1[i] === array2[i]) {
-      rounds += 1;
-      roundsWon += 1;
-      notice.innerText = "Nice, next round";
-    } else {
-      notice.innerText = "Sorry try again";
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log("failure");
+      notice.innerText = "Sorry, try again";
+      // startButton.style.display = 'none'
+      return false;
     }
   }
+  notice.innerText = "Next tound";
+  rounds += 1;
+  cubeArrayCopy = [];
+  setTimeout(computerTurn, 1000);
+}
+
+function resetBoard() {
+  window.location.reload(true);
 }
 
 function showRandomCube() {
@@ -39,13 +44,6 @@ function showRandomCube() {
   newCube.classList.add("red");
   cubeArray.push(newCube);
   console.log(cubeArray);
-}
-
-function changeUnchange() {
-  for (let i = 0; i < rounds; i++) {
-    showRandomCube();
-    hideRandomCube();
-  }
 }
 
 function hideRandomCube() {
@@ -58,65 +56,65 @@ function hideRandomCube() {
   }, 500);
 }
 
+function showHideCube() {
+  showRandomCube();
+  hideRandomCube();
+}
+
 function playerTurn() {
+  clickCount = 0;
   cubes.forEach((cube) => {
     cube.addEventListener("click", (e) => {
-      while (clickCount != cubeArray.length) {
+      while (clickCount !== cubeArray.length) {
         click = e.target;
         console.log(click);
         clickCount += 1;
         cubeArrayCopy.push(click);
       }
-
-      if (compareArrays(cubeArray, cubeArrayCopy)) {
-        notice.innerText = "Nice, next round!";
-        roundsWon += 1;
-        rounds += 1;
-
-        return;
-      } else {
-        notice.innerText = "Sorry, try again!";
-        return;
+      if (clickCount === cubeArray.length) {
+        return arraysAreEqual(cubeArray,cubeArrayCopy)
       }
     });
   });
 }
 
-// function playerTurn () {
-//   cubes.forEach(cube => {
-//     cube.addEventListener('click', e => {
-//       for (let i = 0; i < cubeArray.length; i++){
+// function playerTurn2() {
+//   clickCount = 0;
+//   cubes.forEach((cube) => {
+//     cube.addEventListener("click", (e) => {
+//       for (let i = 0; clickCount < cubeArray.length; i++) {
 //         click = e.target;
+//         clickCount += 1;
 //         console.log(click);
-//         cubeArrayCopy.push(click)
-//         return cubeArrayCopy
+//         cubeArrayCopy.push(click);
 //       }
-//     })
-//   })
+//       clickCount = 0;
+//     });
+//   });
 // }
 
-function nextRound() {
-  for (let i = 0; i < cubeArray.length; i++) {
-    setTimeout(() => {
-      cubeArray[i].classList.add("red");
-      setTimeout(() => {
-        cubeArray[i].classList.remove("red");
-      }, 500);
-    }, i * 500);
-  }
-}
+// function playerTurn() {
+//   clickCount = 0;
+//   cubes.forEach((cube) => {
+//     cube.addEventListener("click", (e) => {
+//       for (let i = 0; clickCount < cubeArray.length; i++) {
+//         click = e.target;
+//         clickCount += 1;
+//         console.log(click);
+//         console.log(clickCount);
+//         cubeArrayCopy.push(click);
+//       }
+//     });
+//   });
+// }
 
-function logRound () {
-  console.log('hi')
-}
-
-function nextRoundPlus() {
+function computerTurn() {
   for (let i = 0; i < cubeArray.length; i++) {
     setTimeout(() => {
       cubeArray[i].classList.add("red");
       console.log(cubeArray[i]);
       if (i === cubeArray.length - 1) {
-        setTimeout(changeUnchange,1000)
+        setTimeout(showHideCube, 1000);
       }
       setTimeout(() => {
         cubeArray[i].classList.remove("red");
@@ -125,29 +123,22 @@ function nextRoundPlus() {
   }
 }
 
-// function reiterateSequence() {
-//   let i = 0;
-
-//   const interval = setInterval(() => {
-//     console.log(cubeArray[i++]);
-//     for (let i = 0; i < cubeArray.length; i++) {
-//       setTimeout(() => {
-//         cubeArray[i].classList.add("red");
-
-//         setTimeout(() => {
-//           cubeArray[i].classList.remove("red");
-//         }, i * 500);
-//       }, i * 1000);
-//     }
-
-//     if (i === cubeArray.length) {
-//       clearInterval(interval);
-//     }
-//   }, 1000);
-// }
-
-startButton.addEventListener("click", (e) => {
-  changeUnchange();
+function startGame() {
+  // startButton.style.display = 'none'
+  showHideCube();
   playerTurn();
-  // compareArrays(cubeArray, cubeArrayCopy)
-});
+}
+
+startButton.addEventListener("click", startGame);
+
+function handleClick(e) {
+  cubes.forEach((cube) => {
+    cube.addEventListener("click", (e) => {
+      for (let i = 0; i < cubeArray.length; i++) {
+        click = e.target;
+        console.log(click);
+        cubeArrayCopy.push(click);
+      }
+    });
+  });
+}
